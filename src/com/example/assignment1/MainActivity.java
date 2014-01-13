5a0b27faa2d35c34c7a9bf1e7388b2ac3715b84e
chunhanlee <chunhan@ualberta.ca>
2014-01-12 18:56:55 -0700

    Created a simple clicker and dynamically changing text view

--- src/com/example/assignment1/MainActivity.java
+++ src/com/example/assignment1/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.assignment1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	TextView count;
+	int curCount = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		count = (TextView) findViewById(R.id.countView);
+		Button button = (Button) findViewById(R.id.clicker);
+		
+		button.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +28,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		curCount++;
+		count.setText(curCount + " times!");
 	}
 
 }
