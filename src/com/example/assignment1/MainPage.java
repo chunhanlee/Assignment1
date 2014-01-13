0e8fa26a2ddd23ad0299712e8e4e327d1ae04975
chunhanlee <chunhan@ualberta.ca>
2014-01-12 19:16:02 -0700

    Duplicated activity_main and MainActivity files to be renamed as MainPage files

---
+++ src/com/example/assignment1/MainPage.java
@@ -0,0 +1,40 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainPage extends Activity implements OnClickListener {
+
+	TextView count;
+	int curCount = 0;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_page);
+		
+		count = (TextView) findViewById(R.id.countView);
+		Button button = (Button) findViewById(R.id.clicker);
+		
+		button.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		curCount++;
+		count.setText(curCount + " times!");
+	}
+
+}
