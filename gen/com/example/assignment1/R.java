5ae73a4e88d28ce3ac5c41996367f1c479ac9dd8
chunhanlee <chunhan@ualberta.ca>
2014-01-12 21:33:24 -0700

    Added new buttons on main page

--- gen/com/example/assignment1/R.java
+++ gen/com/example/assignment1/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080002;
         public static final int clicker=0x7f080001;
         public static final int countView=0x7f080000;
     }
@@ -37,8 +38,10 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int addCounter=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int counter=0x7f050004;
+        public static final int editButton=0x7f050005;
         public static final int hello_world=0x7f050002;
         public static final int whatsup1=0x7f050003;
     }
