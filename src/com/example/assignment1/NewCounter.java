1a07afa680c39b96823d7c5992f6f01024e701b2
chunhanlee <chunhan@ualberta.ca>
2014-01-18 16:51:57 -0700

    added a read/write function on new counter page as a test

--- src/com/example/assignment1/NewCounter.java
+++ src/com/example/assignment1/NewCounter.java
@@ -1,16 +1,33 @@
 package com.example.assignment1;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewCounter extends Activity implements OnClickListener {
-
+	
+	private String FILENAME;
+	private EditText counterName;
+	private String ncount;
+	private FileOutputStream fos;
+	private FileInputStream fis;
+	private InputStreamReader isr;
+	private BufferedReader br;
+	private String cName;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,9 +39,12 @@ public class NewCounter extends Activity implements OnClickListener {
 		Button cancelbutton = (Button) findViewById(R.id.cancelButton);
 		cancelbutton.setOnClickListener(this);
 		
-		//final EditText counterName = (EditText) findViewById(R.id.counterView);
+		Button readB = (Button) findViewById(R.id.readB);
+		readB.setOnClickListener(this);
 		
-		//String FILENAME = counterName;
+		counterName = (EditText) findViewById(R.id.counterName);
+		cName = counterName.getText().toString();
+		FILENAME = cName + ".txt";
 		
 	}
 
@@ -37,9 +57,49 @@ public class NewCounter extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		Intent intent = new Intent(arg0.getContext(), MainPage.class);
-		startActivityForResult(intent, 0);
-	}
+		switch(arg0.getId())
+		{
+		case R.id.cancelButton:
+			Intent intent = new Intent(arg0.getContext(), MainPage.class);
+			startActivityForResult(intent, 0);
+			
+		case R.id.saveButton:
+			counterName = (EditText) findViewById(R.id.counterName);
+			cName = counterName.getText().toString();
+			FILENAME = cName + ".txt";
+			
+			ncount = cName + " Count: 0";
+			try {
+				fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+				fos.write(ncount.getBytes());
+				fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//Intent intent1 = new Intent(arg0.getContext(), MainPage.class);
+			//startActivityForResult(intent1, 0);
+			break;
+		case R.id.readB:
+			counterName = (EditText) findViewById(R.id.counterName);
+			cName = counterName.getText().toString();
+			FILENAME = cName + ".txt";
+			try {
+				fis = openFileInput(FILENAME);
+				isr = new InputStreamReader(fis);	
+				br = new BufferedReader(isr);
+			
+				String sLine = null;
+				String out = "";
+				while ((sLine = br.readLine())!=null){
+					out += sLine;
+				}
+				Toast.makeText(this, out, Toast.LENGTH_LONG).show();
+				} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}		
 
 }
